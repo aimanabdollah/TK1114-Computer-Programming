5e13fb8555faa4f9f3b9526c894fa1ea66c75414
Muhammad Aiman <64502066+aimanabdollah@users.noreply.github.com>
2020-09-07 09:36:16 +0800

    Update F_HerbalSalesman.java

--- 4. Array and Array Processing 1/F_HerbalSalesman.java
+++ 4. Array and Array Processing 1/F_HerbalSalesman.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 public class F_HerbalSalesman {
-
 	public static void main(String[] args) {
 
 		Scanner sc=new Scanner(System.in);
@@ -10,41 +9,30 @@ public class F_HerbalSalesman {
 		for(int k=0; k<testCase; k++)
 		{
 			int days = sc.nextInt();
-			
 			int[] arrProduct = new int[days];
-			
 			for(int i=0; i<days; i++) {
 				arrProduct[i] = sc.nextInt();
 			}
 			
-			
 			int max = arrProduct[0], min = arrProduct[0], countMax = 0, countMin = 0;
 			
-			for(int i=0; i<arrProduct.length; i++) {
-				
+			for(int i=0; i<arrProduct.length; i++) {	
 				if(max<arrProduct[i])
 					max = arrProduct[i];
-
 			}
 			
-			for(int i=0; i<arrProduct.length; i++) {
-				
+			for(int i=0; i<arrProduct.length; i++) {	
 				if(min>arrProduct[i])
 					min = arrProduct[i];
 			}
 			
-			for(int i=0; i<arrProduct.length; i++) {
-				
+			for(int i=0; i<arrProduct.length; i++) {	
 				if(max==arrProduct[i])
-					countMax++;
-					
+					countMax++;	
 				else if(min==arrProduct[i])
 					countMin++;	
 			}
-			
-			System.out.println("Case #"+(k+1)+": "+min + " " + countMin + " " + max + " "+ countMax);
-				
-			}
+			System.out.println("Case #"+(k+1)+": "+min + " " + countMin + " " + max + " "+ countMax);		
 		}
-		
+	}		
 }
