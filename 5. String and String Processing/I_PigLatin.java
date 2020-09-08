608e2682d9f895d0fcf5e1338b7cde0678d0bfd1
Muhammad Aiman <64502066+aimanabdollah@users.noreply.github.com>
2020-09-08 10:42:47 +0800

    Update I_PigLatin.java

--- 5. String and String Processing/I_PigLatin.java
+++ 5. String and String Processing/I_PigLatin.java
@@ -9,23 +9,23 @@ public class I_PigLatin
        s.nextLine();
        for(i=0;i<n;i++)
        {
-       str=s.nextLine();//read the String
-       result="";
-       String []strarr=str.split(" ");//if it has more than one word spilt at spacs
-       for (String t: strarr)//for each string process latin
-       {
-       char ch=t.charAt(0);//first Character of word
-       if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
-       {
-       //if it is vowel just add "ay"
-       temp=t+"ay";
-       }
-       else//else move thtemp=t+"ay"; first to last using substring and add ay
-       temp=t.substring(1)+ch+"ay";
+           str=s.nextLine();//read the String
+           result="";
+           String []strarr=str.split(" ");//if it has more than one word spilt at spacs
+           for (String t: strarr)//for each string process latin
+           {
+               char ch=t.charAt(0);//first Character of word
+               if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+               {
+                //if it is vowel just add "ay"
+                temp=t+"ay";
+               }
+               else//else move thtemp=t+"ay"; first to last using substring and add ay
+                  temp=t.substring(1)+ch+"ay";
       
-       result+=" "+temp;//result eill store all the converted words as a single sentence
-       }
-       System.out.println("case #"+(i+1)+":"+result);//print result
+               result+=" "+temp;//result eill store all the converted words as a single sentence
+           }
+         System.out.println("case #"+(i+1)+":"+result);//print result
        }
    }
 }
